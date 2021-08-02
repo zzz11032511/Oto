c49be27739f093635643f0b21efe207dd88858b6
i18furuta@tokuyama.kosen-ac.jp <fn.nk.2002@gmail.com>
2021-08-02 21:35:39 +0900

    support if-then

--- src/parser/control.c
+++ src/parser/control.c
@@ -39,17 +39,67 @@ int32_t getArgs(tokenBuf_t *tcBuf, int32_t start, int32_t end, int32_t *argv, in
     return argCnt;
 }
 
+/* "then"の位置を渡すと、それに対応した"elsif", "else", "end"の位置を返す */
+int32_t searchIFBlockEnd(tokenBuf_t *tcBuf, int32_t pc) {
+    int32_t nest = 1;
+
+    while (1) {
+        int32_t tc = tcBuf->tc[pc++];
+        if (tc == TcIf || tc == TcBegin) {
+            nest++;
+            continue;
+        } else if (tc == TcElsif && nest == 1) {
+            break;
+        } else if (tc == TcElse && nest == 1) {
+            break;
+        } else if (tc == TcEnd) {
+            nest--;
+            if (nest != 0) continue;
+            else break;
+        } else if (tc == TcExit) {
+            callError(INVALID_SYNTAX_ERROR);
+        }
+    }
+
+    return pc - 1;
+}
+
+int32_t searchElseBlockEnd(tokenBuf_t *tcBuf, int32_t pc) {
+    int32_t nest = 1;
+
+    while (1) {
+        int32_t tc = tcBuf->tc[pc++];
+        if (tc == TcIf || tc == TcBegin) {
+            nest++;
+            continue;
+        } else if (tc == TcEnd) {
+            nest--;
+            if (nest != 0) continue;
+            else break;
+        } else if (tc == TcExit) {
+            callError(INVALID_SYNTAX_ERROR);
+        }
+    }
+
+    return pc - 1;
+}
 
 /* "begin", '['の位置を渡すと、それに対応した"end", ']'の位置を返す */
 int32_t searchBlockEnd(tokenBuf_t *tcBuf, int32_t pc) {
     int32_t nest = 0;
 
+    if (tcBuf->tc[pc] == TcThen) {
+        return searchIFBlockEnd(tcBuf, pc);
+    } else if (tcBuf->tc[pc] == TcElse) {
+        return searchElseBlockEnd(tcBuf, pc);
+    }
+
     while (1) {
         int32_t tc = tcBuf->tc[pc++];
-        if (tc == TcBegin || tc == TcSqBrOpn || tc == TcThen) {
+        if (tc == TcBegin || tc == TcSqBrOpn) {
             nest++;
             continue;
-        } else if (tc == TcEnd || tc == TcSqBrCls || tc == TcElsif || tc == TcElse) {
+        } else if (tc == TcEnd || tc == TcSqBrCls) {
             nest--;
             if (nest != 0) continue;
             else break;
@@ -107,20 +157,22 @@ void ifControl(tokenBuf_t *tcBuf, int32_t *icp, int32_t *pc, var_t *var, var_t *
         ppc++;
     }
 
+    // 条件式部分
     int32_t start = ppc;
     int32_t end = searchBlockEnd(tcBuf, start);
     start++;
 
     expr(tcBuf, icp, &start, var, ic, end - 1);
-
     ppc = end + 1;
 
+    // 処理部分
     int32_t jmpIcp1 = *icp;
     putIc(ic, icp, OpJz, 0, 0, 0, 0);
 
     ppc = blockCompile(tcBuf, icp, &ppc, var, ic);
 
-    if (tcBuf->tc[ppc] == TcElsif) {
+    if (tcBuf->tc[ppc - 1] == TcElsif) {
+        ppc--;
         int32_t jmpIcp2 = *icp;
         putIc(ic, icp, OpJmp, 0, 0, 0, 0);
 
@@ -133,7 +185,8 @@ void ifControl(tokenBuf_t *tcBuf, int32_t *icp, int32_t *pc, var_t *var, var_t *
         ifControl(tcBuf, icp, &ppc, var, ic);
         putIc(ic, &jmpIcp2, OpJmp, (var_t *)((int64_t)*icp), 0, 0, 0);
 
-    } else if (tcBuf->tc[ppc] == TcElse) {
+    } else if (tcBuf->tc[ppc - 1] == TcElse) {
+        ppc--;
         int32_t jmpIcp3 = *icp;
         putIc(ic, icp, OpJmp, 0, 0, 0, 0);
 
@@ -142,8 +195,6 @@ void ifControl(tokenBuf_t *tcBuf, int32_t *icp, int32_t *pc, var_t *var, var_t *
         } else {
             putIc(ic, &jmpIcp1, OpJz, (var_t *)((int64_t)*icp), 0, 0, 0);
         }
-
-        ppc++; // else { の分2個進める
 
         ppc = blockCompile(tcBuf, icp, &ppc, var, ic);
 
