527b546bf3b18016a56a4cea950df85918fa6cc0
i18furuta <fn.nk.2002@gmail.com>
2021-10-24 15:36:51 +0900

    [REDESIGN]Fix otomath.h

--- src/soundver2/otomath.h
+++ src/soundver2/otomath.h
@@ -2,6 +2,14 @@
 
 #include <math.h>
 
-#define PI          3.14159265358979323846
-#define M_SQRT2		1.41421356237309504880
-#define M_SQRT1_2	0.70710678118654752440
+#ifndef PI
+    #define PI          3.14159265358979323846
+#endif
+
+#ifndef M_SQRT2
+    #define M_SQRT2		1.41421356237309504880
+#endif
+
+#ifndef M_SQRT1_2
+    #define M_SQRT1_2	0.70710678118654752440
+#endif
